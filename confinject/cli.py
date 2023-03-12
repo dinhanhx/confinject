40ccd213136a6f139d03a7f026ee965c5a33d988
Dinh Anh <dinhanhx@gmail.com>
2023-03-12 10:40:44 +0700

    Format document

--- confinject/cli.py
+++ confinject/cli.py
@@ -27,12 +27,18 @@ else:
 
 if not CWD.joinpath('.vscode/settings.json').is_file():
     print('Create .vscode/settings.json')
-    shutil.copy(TEMPLATE_DIR.joinpath('.vscode/settings.json'), CWD.joinpath('.vscode/settings.json'))
+    shutil.copy(
+        TEMPLATE_DIR.joinpath('.vscode/settings.json'),
+        CWD.joinpath('.vscode/settings.json'),
+    )
 else:
     print('.vscode/settings.json exists already')
 
 if not CWD.joinpath('.vscode/extensions.json').is_file():
     print('Create .vscode/extensions.json')
-    shutil.copy(TEMPLATE_DIR.joinpath('.vscode/extensions.json'), CWD.joinpath('.vscode/extensions.json'))
+    shutil.copy(
+        TEMPLATE_DIR.joinpath('.vscode/extensions.json'),
+        CWD.joinpath('.vscode/extensions.json'),
+    )
 else:
     print('.vscode/extensions.json exists already')
